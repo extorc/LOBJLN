09468807a81873d49eae8f97070eb11a733a885b
Shaurya Mittal <mittalshaurya004@gmail.com>
2022-10-04 11:31:27 +0530

    Data conversion to float

--- src/main.cpp
+++ src/main.cpp
@@ -14,26 +14,37 @@ int main () {
     {
       char *cline = &line[0];
 
+      std::vector<std::string> posList = StringParser::SeekAndTokenize(line, 'v', ' '); //Isolating Portion of OBJ file which starts with 'v ' 
+      std::vector<float> posListFloat;
+      StringParser::EliminateLabels(posList, 'v', ' ');                                 //removing the 'v ' labels before data conversion
 
-      std::vector<std::string> posList = StringParser::SeekAndTokenize(line, 'v', ' ');
-      StringParser::EliminateLabels(posList, 'v', ' ');
-
+      for(int i = 0; i < posList.size(); i++){                                          //data conversion
+        posListFloat.push_back(std::stof(posList.at(i)));
+      }
       for(int i = 0; i < posList.size(); i++){
-        std::cout<<posList.at(i)<<std::endl;
+        std::cout<<posListFloat.at(i)<<std::endl;                                       //debugging output
       }
 
       std::vector<std::string> normList = StringParser::SeekAndTokenize(line, 'v', 'n');
+      std::vector<float> normListFloat;
       StringParser::EliminateLabels(normList, 'v', 'n');
 
       for(int i = 0; i < normList.size(); i++){
-        std::cout<<normList.at(i)<<std::endl;
+        normListFloat.push_back(std::stof(normList.at(i)));
+      }
+      for(int i = 0; i < normList.size(); i++){
+        std::cout<<normListFloat.at(i)<<std::endl;
       }
 
       std::vector<std::string> texList = StringParser::SeekAndTokenize(line, 'v', 't');
+      std::vector<float> texListFloat;
       StringParser::EliminateLabels(texList, 'v', 't');
 
       for(int i = 0; i < texList.size(); i++){
-        std::cout<<texList.at(i)<<std::endl;
+        texListFloat.push_back(std::stof(texList.at(i)));
+      }
+      for(int i = 0; i < texList.size(); i++){
+        std::cout<<texListFloat.at(i)<<std::endl;
       }
 
       std::vector<std::string> funcList = StringParser::SeekAndTokenize(line, 'f', ' ');
